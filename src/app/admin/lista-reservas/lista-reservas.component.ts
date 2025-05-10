e390fe49bcba4fc581f7d477b532993b50fea5c9
Angel <113852397+AngelOncoy@users.noreply.github.com>
2025-05-10 00:31:55 -0500

    feat: estructura inicial del front

---
+++ src/app/admin/lista-reservas/lista-reservas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-reservas',
+  imports: [],
+  templateUrl: './lista-reservas.component.html',
+  styleUrl: './lista-reservas.component.css'
+})
+export class ListaReservasComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CartaComponent} from './home/components/carta/carta.component';
+import {ReservasComponent} from './reservas/reservas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'carta', component: CartaComponent},
+  {path: 'reservas', component: ReservasComponent},
+];
