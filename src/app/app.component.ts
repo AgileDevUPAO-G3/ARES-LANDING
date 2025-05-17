441450a3891c19b43805654a52450cd2400f1c3e
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-05-16 23:17:55 -0500

    Style: Registros de datos de reserva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import {NavbarComponent} from './shared/components/navbar/navbar.component';
 import {HomeComponent} from './pages/home/home.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
 import {ReservasComponent} from './pages/reservas/reservas.component';
+import {PagosComponent } from './pages/pagos/pagos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, FooterComponent, ReservasComponent, HomeComponent],
+  imports: [NavbarComponent, FooterComponent, ReservasComponent, PagosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
